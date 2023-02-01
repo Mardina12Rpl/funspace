900dc85b80e5d53197295091dc3c7e7b90dd078b
Mardina12Rpl <124115545+Mardina12Rpl@users.noreply.github.com>
2023-01-31 22:09:01 -0800

    Add files via upload

---
+++ f1.py
@@ -0,0 +1,17 @@
+#Hitung Luas Segitiga Sederhana
+def Luas_segitiga();
+    a = int(input("Masukkan alas segitiga: "))
+    t = int(input("Masukkan tinggi segitiga"))
+    luas = a * t / 2
+    print("Luas segitiga adalah: ", luas)
+
+    luas_segitiga()
+
+    # Hitung Luas Persegi Panjang
+    def luas_persegi_panjang():
+        p = int (input("Masukkan Panjang Persegi"))
+        l = int (input("Masukkan Lebar Persegi"))
+
+        luas = p * l
+        print("Luas Persegi adalah: ", luas)
+    luas_persegi_panjang
